faf85cbc5e25ef4a2723044e211ac4c27fa52967
MAhmedk10 <156852065+MAhmedk10@users.noreply.github.com>
2024-03-22 03:11:48 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# cmndlinecalculator-cli" 
---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+// SHABANG
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "SecondNumber" },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: [
+      "Addition",
+      "Subtraction",
+      "Multiplication",
+      "Division",
+      "Exponent",
+    ],
+  },
+]);
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.SecondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.SecondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.SecondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.SecondNumber);
+} else if (answer.operator === "Exponent") {
+  console.log(answer.firstNumber ** answer.SecondNumber);
+} else {
+  ("Please select a valid Number");
+}
